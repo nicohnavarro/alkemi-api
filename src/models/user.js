be1cee2a346bf78cf94eaf591523d35d58091629
nicohnavarro <nicohnavarro@gmail.com>
2021-09-21 13:07:08 -0300

    :rocket:

--- src/models/user.js
+++ src/models/user.js
@@ -1,45 +1,45 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-const mongoosePaginate = require('mongoose-paginate-v2');
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const mongoosePaginate = require("mongoose-paginate-v2");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: {
       type: String,
-      required: [true,'Name required']
+      required: [true, "Name required"],
     },
     lastName: {
-      type:String,
-      required: [true,'Last Name required']
+      type: String,
+      required: [true, "Last Name required"],
     },
     email: {
-      type:String,
-      required:[true,'Email required'],
-      unique:true,
-      index:true
+      type: String,
+      required: [true, "Email required"],
+      unique: true,
+      index: true,
     },
     dateOfBirth: Date,
     password: {
-      type:String,
-      required:[true,'Password required']
+      type: String,
+      required: [true, "Password required"],
     },
     role: {
-      type:String,
-      required:[true],
-      default: 'USER_ROLE',
-      enum: ['USER_ROLE','ADMIN_ROLE']
+      type: String,
+      required: [true],
+      default: "USER_ROLE",
+      enum: ["USER_ROLE", "ADMIN_ROLE"],
     },
     enable: {
       type: Boolean,
       required: [true],
-      default: true
-    }
+      default: true,
+    },
   },
-  {timestamps:true}
+  { timestamps: true }
 );
 
-userSchema.plugin(uniqueValidator, {message:'already exist in the DB'});
+userSchema.plugin(uniqueValidator, { message: "already exist in the DB" });
 userSchema.plugin(mongoosePaginate);
 
-module.exports = mongoose.model('users',userSchema);
-
+module.exports = mongoose.model("users", userSchema);
