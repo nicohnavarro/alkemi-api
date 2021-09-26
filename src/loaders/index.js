447cd911622bd3200ee72fc019706cc9a0d53838
nicohnavarro <nicohnavarro@gmail.com>
2021-09-26 01:15:43 -0300

    :rocket: new database

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,19 +1,22 @@
-const ExpressServer = require('./server/expressServer');
-const mongooseLoader = require('./mongoose');
-const config = require('../config');
-const logger= require('./logger');
+const ExpressServer = require("./server/expressServer");
+const sequelize = require("./sequelize");
+const config = require("../config");
+const logger = require("./logger");
 
-const startServer = async ()=>{
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    logger.info("Database Loaded and Connected");
 
-  await mongooseLoader();
-  logger.info('Database Loaded and Connected');
+    const server = new ExpressServer();
+    logger.info("Express Loaded");
+    server.start();
+    logger.info("#############################################");
+    logger.info(`Server listening on port: ${config.port}`);
+    logger.info("#############################################");
+  } catch (err) {
+    console.error("Unable to connect to the database!", err);
+  }
+};
 
-  const server = new ExpressServer();
-  logger.info('Express Loaded');
-  server.start();
-  logger.info('#############################################');
-  logger.info(`Server listening on port: ${config.port}`);
-  logger.info('#############################################');
-}
- 
 module.exports = startServer;
