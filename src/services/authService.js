18b4c002383642d41f83d1172d19f26ee5d3d633
nicohnavarro <nicohnavarro@gmail.com>
2021-09-26 18:33:00 -0300

    :fire: finish register

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,109 +1,98 @@
-import { compare } from 'bcrypt';
-import JWT  from 'jsonwebtoken';
-import { auth } from '../config/index.js';
-import { findByEmail, findById } from './userService.js';
-import AppError from '../errors/appError.js';
+import { compare } from "bcrypt";
+import JWT from "jsonwebtoken";
+import { auth } from "../config/index.js";
+import { findByEmail, findById } from "./userService.js";
+import AppError from "../errors/appError.js";
+import UserRepository from "../repositories/userRepository.js";
+const repository = new UserRepository();
 
-const login = async(email,password) => {
-  try{
+const login = async (email, password) => {
+  try {
     const user = await findByEmail(email);
 
-    if(!user){
-      throw new AppError('User not found!',401)
+    if (!user) {
+      throw new AppError("User not found!", 401);
     }
 
-    if(!user.enable){
-      throw new AppError('User disabled!',401)
+    if (!user.enable) {
+      throw new AppError("User disabled!", 401);
     }
 
     const validPassword = await compare(password, user.password);
-    
-    if(!validPassword){
-      throw new AppError('Authentication failed, Password invalid',401);
+
+    if (!validPassword) {
+      throw new AppError("Authentication failed, Password invalid", 401);
     }
 
     const token = _encrypt(user.id);
 
-    return { 
+    return {
       token,
-      user:user.name,
-      role:user.role
-    }
-  }catch(err){
+      user: user.name,
+      role: user.role,
+    };
+  } catch (err) {
     throw err;
   }
-}
+};
 
-const register = async(name,username,email,password) => {
-  try{
+const register = async (name, username, email, password) => {
+  try {
     const user = await findByEmail(email);
 
-    if(!user){
-      throw new AppError('User not found!',401)
+    if (user) {
+      throw new AppError("Mail already use!", 401);
     }
-
-    if(!user.enable){
-      throw new AppError('User disabled!',401)
-    }
-
-    const validPassword = await compare(password, user.password);
-    
-    if(!validPassword){
-      throw new AppError('Authentication failed, Password invalid',401);
-    }
-
-    const token = _encrypt(user.id);
-
-    return { 
-      token,
-      user:user.name,
-      role:user.role
-    }
-  }catch(err){
+    const newUser = {
+      name,
+      username,
+      email,
+      password,
+    };
+    const returnUser = await repository.save(newUser);
+    return {
+      user: returnUser.toJSON(),
+      role: returnUser.toJSON().role,
+    };
+  } catch (err) {
     throw err;
   }
-}
+};
 
-const validToken = async(token) => {
-  try{
-    if(!token){
-      throw new AppError('Authentication failed, token required',401);
+const validToken = async (token) => {
+  try {
+    if (!token) {
+      throw new AppError("Authentication failed, token required", 401);
     }
     let id;
-    try{
-      const obj =  JWT.verify(token,auth.secret);
+    try {
+      const obj = JWT.verify(token, auth.secret);
       id = obj.id;
-    }catch(verifyErr){
-      throw new AppError('Authentication failed, Error token',402);
+    } catch (verifyErr) {
+      throw new AppError("Authentication failed, Error token", 402);
     }
     const user = await findById(id);
-    if(!user){
-      throw new AppError('Authentication failed, Invalid token',401);
+    if (!user) {
+      throw new AppError("Authentication failed, Invalid token", 401);
     }
-    if(!user.enable){
-      throw new AppError('Authentication failed, User is not enable',401);
+    if (!user.enable) {
+      throw new AppError("Authentication failed, User is not enable", 401);
     }
-    return user
-  }
-  catch(err){
+    return user;
+  } catch (err) {
     throw err;
   }
-}
+};
 
 const validRole = (user, ...roles) => {
-  if(!roles.includes(user.role)){
-    throw new AppError('Authorization failed, User without permision',403);
+  if (!roles.includes(user.role)) {
+    throw new AppError("Authorization failed, User without permision", 403);
   }
   return true;
-}
+};
 
 const _encrypt = (id) => {
-  return JWT.sign({ id }, auth.secret,{ expiresIn:auth.ttl});
-}
+  return JWT.sign({ id }, auth.secret, { expiresIn: auth.ttl });
+};
 
-export {
-  login,
-  register,
-  validToken,
-  validRole
-}
+export { login, register, validToken, validRole };
